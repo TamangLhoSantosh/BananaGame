dcdb51330861ec127c38eb8ab239e683e0c87d35
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-10-29 07:06:19 +0545

    project setup

---
+++ src/main/java/com/tamanglhosantosh/bananagame/BananaGameApplication.java
@@ -0,0 +1,13 @@
+package com.tamanglhosantosh.bananagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananaGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamanglhosantosh/bananagame/BananaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamanglhosantosh.bananagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BananaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
